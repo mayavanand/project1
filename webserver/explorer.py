ded604585f1d797bd755a20309fe9658fc116641
mayavanand <mva2112@instance-1.c.sigma-current-145318.internal>
2016-10-30 03:00:34 +0000

    homepage bare bones

--- webserver/explorer.py
+++ webserver/explorer.py
@@ -134,7 +134,8 @@ def index():
   # render_template looks in the templates/ folder for files.
   # for example, the below file reads template/index.html
   #
-  return render_template("index.html", **context)
+  #return render_template("index.html", **context)
+  return render_template("index.html")
 @app.route('/group/Hair_Color')
 def group():
   category = "Hair Color"
