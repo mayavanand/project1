1f9fb377a230c960a1e8ac841cb9605dbd11df75
mayavanand <mva2112@instance-1.c.sigma-current-145318.internal>
2016-11-13 02:37:32 +0000

    added login and signup pages

--- webserver/explorer.py
+++ webserver/explorer.py
@@ -18,37 +18,57 @@ Read about it online.
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, request, render_template, g, redirect, Response, session, url_for, escape, flash
+from hashlib import md5
 from flask_bootstrap import Bootstrap
 from flask_nav import Nav
-from flask_nav.elements import Navbar, View
+from flask_nav.elements import *
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import Required, Email
 
-
+#Create app and configure boostrap
 def create_app():
     tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
     app = Flask(__name__, template_folder=tmpl_dir)
+    app.config['SECRET_KEY'] = 'apple-pineapple-cle'
     Bootstrap(app)
     return app
-app = create_app()
-nav = Nav()
-DATABASEURI = "postgres://mva2112:n3dek@104.196.175.120/postgres"
 
-# This line creates a database engine that knows how to connect to the URI above
+app = create_app()
+
+#Create navbar
+topbar = Navbar('', View('Home', 'index'), View('Groups', 'groups'), View('Login', 'login'), View('Log Out', 'logout'), View('Sign Up', 'signup'), )
+
+nav = Nav()
+nav.register_element('top', topbar)
+nav.init_app(app)
+
+DATABASEURI = "postgres://mva2112:n3dek@104.196.175.120/postgres"
 engine = create_engine(DATABASEURI)
+
+class ServerError(Exception):pass
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Email()])
+    password = PasswordField('Password', validators=[Required()])
+    submit = SubmitField('Log In')
+
+class SignupForm(FlaskForm):
+    email = StringField('Email', validators=[Required(), Email()])
+    password = PasswordField('Password', validators=[Required()])
+    institution = StringField('Institution (optional)')
 
 @app.before_request
 def before_request():
   """
-  This function is run at the beginning of every web request 
-  (every time you enter an address in the web browser).
-  We use it to setup a database connection that can be used throughout the request
-
+  setup a database connection that can be used throughout the request
   The variable g is globally accessible
   """
   try:
     g.conn = engine.connect()
   except:
-    print "uh oh, problem connecting to database"
+    print "problem connecting to database"
     import traceback; traceback.print_exc()
     g.conn = None
 
@@ -56,96 +76,19 @@ def before_request():
 def teardown_request(exception):
   """
   At the end of the web request, this makes sure to close the database connection.
-  If you don't the database could run out of memory!
   """
   try:
     g.conn.close()
   except Exception as e:
     pass
 
-@nav.navigation()
-def mynavbar():
-    return Navbar(
-        'mysite',
-        View('Home', 'index'),
-    )
-
-nav.init_app(app)
-
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to e.g., localhost:8111/foobar/ with POST or GET then you could use
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
 @app.route('/')
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
-
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
-
-  # DEBUG: this is debugging code to see what request looks like
-  print request.args
-
-
-  #
-  # example of a database query
-  #
-  cursor = g.conn.execute("SELECT email FROM users;")
-  names = []
-  for result in cursor:
-    names.append(result['email'])  # can also be accessed using result[0]
-  cursor.close()
-
-
-
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = names)
-
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html")
+    if 'username' in session:
+        username_session = escape(session['username']).capitalize()
+    else:
+        username_session = ""
+    return render_template("index.html", session_user_name=username_session)
 
 @app.route('/groups')
 def groups():
@@ -166,6 +109,7 @@ def singleGroup(category):
   cursor.close()
   context = dict(data = rsid)
   return render_template("groupRSID.html", **context)
+
 @app.route('/<rsid>')
 def variant(rsid):
   variant = []
@@ -192,17 +136,6 @@ def variant(rsid):
   cursor.close()
   context = dict(data = variant)
   return render_template("variantTry2.html", **context)
-#
-# This is an example of a different path.  You can see it at
-# 
-#     localhost:8111/another
-#
-# notice that the functio name is another() rather than index()
-# the functions for each app.route needs to have different names
-#
-@app.route('/another')
-def another():
-  return render_template("anotherfile.html")
 
 
 # Example of adding new data to the database
@@ -213,12 +146,75 @@ def add():
   return redirect('/')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    abort(401)
-    this_is_never_executed()
+    form = LoginForm()    
+    if 'username' in session:
+        return redirect(url_for('index'))
+    
+    error = None
+    try:
+        if request.method == 'POST':
+            username_form = '%'+ form.email.data + '%'
+            cmd = 'SELECT COUNT(1) FROM users WHERE email like :email1'
+            cursor = g.conn.execute(text(cmd), email1 = username_form)
+        
+            if not cursor.fetchone()[0]:
+                raise ServerError('Invalid username')
+                flash('USER NOT FOUND')
+        
+            password_form = form.password.data            
+            cmd = 'SELECT password_hash FROM users where email like :email1'
+            cursor = g.conn.execute(text(cmd), email1 = username_form)
+        
+            for row in cursor.fetchall():
+                if md5(password_form).hexdigest() == row[0]:
+                    session['username'] = form.email.data
+                    return redirect(url_for('index'))
+            raise ServerError('Invalid password')
 
+    except ServerError as e:
+        error = str(e)
+    flash(error)    
+    return render_template('login.html', form=form, error=error)
+ 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if 'username' in session:
+        return redirect(url_for('index'))
 
+    error = None
+    try:
+        if request.method == 'POST':
+            researcher = False
+            username_form = '%'+ form.email.data + '%'
+            password = md5(form.password.data).hexdigest()
+            if form.institution.data.length > 1:
+                institution = form.institution.data[0]
+                researcher = True
+
+            cmd = 'INSERT into users VALUES (:email1, :pw)'
+            cursor = g.conn.execute(text(cmd), email1 = username_form, pw = password)
+
+            if researcher == True:
+                cmd = 'INSERT into researcher VALUES (:email1, :inst)'
+                cursor = g.conn.execute(text(cmd), email1 = username_form, inst = institution)
+            if researcher == False:
+                cmd = 'INSERT into casual VALUES (:email1)'
+                cursor = g.conn.execute(text(cmd), email1 = username_form)
+
+            session['username'] = form.email.data
+            flash('Account Created')
+            return redirect(url_for('index'))
+            
+    except ServerError as e:
+        error = str(e)
+    return render_template('signup.html', form=form, error=error)
+
+@app.route('/logout')
+def logout():
+    return render_template('groups.html')
 if __name__ == "__main__":
   import click
 
