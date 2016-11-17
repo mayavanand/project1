2a4a337b767c775c46d57a688230bbc4ee542352
mayavanand <mva2112@instance-1.c.sigma-current-145318.internal>
2016-11-17 00:06:01 +0000

    flashed messages for login permissions and format of signup page

--- webserver/explorer.py
+++ webserver/explorer.py
@@ -88,6 +88,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'username' not in session:
+            flash('Log in to see page!')
             return redirect(url_for('login', next=request.url))
         return f(*args, **kwargs)
     return decorated_function
